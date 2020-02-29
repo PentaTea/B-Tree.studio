5e0df6d99fa7f3f2d6770923e0b73239da0cb9e3
moecopilot <zjp11679841@163.com>
2020-02-29 17:57:55 +0800

    草生。

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -52,6 +52,9 @@ module.exports = {
                 items: [{
                     text: '*即将开放*',
                     link: 'index'
+                }, {
+                    text: '*即将开放*',
+                    link: 'index'
                 }]
             }]
         }, {
