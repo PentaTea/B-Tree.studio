476198adbd3363fa1cda1a983849d4cc3526cfc1
PentaTea <2490445193@qq.com>
2020-09-05 17:03:26 +0800

    加了个逗号

--- docs/py-list/py-list.js
+++ docs/py-list/py-list.js
@@ -7,7 +7,7 @@
 module.exports = [
   {
     name: "Wuyongzhi",
-    description: "如果不是为了余生安稳谁愿意颠沛流离",
+    description: "如果不是为了余生安稳,谁愿意颠沛流离",
     url: "https://wuyongzhi.top/",
     logo: "https://s2.ax1x.com/2020/02/19/3ANb1e.jpg",
   },
